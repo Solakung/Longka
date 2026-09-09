f9cc3bcdcf16fe8dfcaa25e29bc7c40712623ceb
Solakung <spiritual936@gmail.com>
2026-09-09 10:53:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'lanka-v1.2'; // ⬆️ 
+const CACHE = 'lanka-v${{ github.sha }}';
 const ASSETS = ['./', 'index.html', 'css/style.css', 'js/game.js', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
